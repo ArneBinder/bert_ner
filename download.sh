5e627dfe140a640a3fd28d8b20d4659b4297053f
Arne Binder <arne.b.binder@gmail.com>
2019-07-12 14:44:23 +0200

    fixed download script

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-train=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/train.txt
-valid=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/valid.txt
-test=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/test.txt
+train=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/a8bc4813b49cafe436cf71b49ef97c5ddae3c78c/neuroner/data/conll2003/en/train.txt
+valid=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/a8bc4813b49cafe436cf71b49ef97c5ddae3c78c/neuroner/data/conll2003/en/valid.txt
+test=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/a8bc4813b49cafe436cf71b49ef97c5ddae3c78c/neuroner/data/conll2003/en/test.txt
 
 mkdir conll2003 | wget --show-progress $train && mv train.txt conll2003
 wget --show-progress $valid && mv valid.txt conll2003
